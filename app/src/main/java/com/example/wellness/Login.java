3620649868ef35e53f9c6d9add3f864f4f54b44b
il42133 <82491273+il42133@users.noreply.github.com>
2022-07-23 23:12:23 +0800

    Update Login.java

--- app/src/main/java/com/example/wellness/Login.java
+++ app/src/main/java/com/example/wellness/Login.java
@@ -64,7 +64,7 @@ public class Login extends AppCompatActivity {
                                     String result = putData.getResult();
                                     if(result.equals("Login Successful")){
                                         Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        Intent intent = new Intent(getApplicationContext(), ActivityList.class);
                                         startActivity(intent);
                                         finish();
                                     }
