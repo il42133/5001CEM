a84e624f92e197154bc1460c8d2fb002642f612e
il42133 <82491273+il42133@users.noreply.github.com>
2022-07-24 12:40:18 +0800

    test

--- app/src/main/java/com/example/wellness/ActivityList.java
+++ app/src/main/java/com/example/wellness/ActivityList.java
@@ -23,7 +23,7 @@ public class ActivityList extends AppCompatActivity {
     List<String> activityList, getActivityListTime;
     String username;
     Button buttonAddActivity;
-    TextInputEditText textInputEditTextActivity, textInputEditTextTime;
+    TextInputEditText activityName, activityTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,42 +52,49 @@ public class ActivityList extends AppCompatActivity {
         final Dialog dialog = new Dialog(ActivityList.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(true);
-        dialog.setContentView(R.layout.activity_list);
+        dialog.setContentView(R.layout.activity_dialog);
 
-        EditText activityName = dialog.findViewById(R.id.activityName);
-        EditText activityTime = dialog.findViewById(R.id.activityTime);
+        activityName = dialog.findViewById(R.id.activityName);
+        activityTime = dialog.findViewById(R.id.activityTime);
         Button confirmActivityButton = dialog.findViewById(R.id.confirm_activity_button);
 
         confirmActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String activityN = activityName.getText().toString();
-                String activityT = activityTime.getText().toString();
+                String activityN = String.valueOf(activityName.getText());
+                String activityT = String.valueOf(activityTime.getText());
+                String usernameL = username;
                 Integer activityTI = Integer.parseInt(activityT);
                 if(!activityN.equals("") && !activityT.equals("")) {
                     Handler handler = new Handler(Looper.getMainLooper());
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            String[] field = new String[2];
+                            String[] field = new String[3];
                             field[0] = "activity";
                             field[1] = "time";
-                            String[] data = new String[2];
+                            field[2] = "username";
+                            String[] data = new String[3];
                             data[0] = activityN;
                             data[1] = activityT;
+                            data[2] = usernameL;
                             PutData putData = new PutData("http://192.168.100.25/5001CEM/activity.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
                                     String result = putData.getResult();
-                                    if(result.equals("Activity Successfully added")){
-                                        PutData putData2 = new PutData("http://192.168.100.25/5001CEM/updatePoints.php", "POST", field, data);
+                                    /*if(result.equals("Activity Successfully added")){
+                                        String[] field2 = new String[1];
+                                        field2[0] = "points";
+                                        String[] data2 = new String[1];
+                                        data2[0] = activityT;
+                                        PutData putData2 = new PutData("http://192.168.100.25/5001CEM/updatePoints.php", "POST", field2, data2);
                                         if (putData2.startPut()) {
                                             if (putData2.onComplete()) {
                                                 String result2 = putData2.getResult();
                                                 Toast.makeText(getApplicationContext(), result2, Toast.LENGTH_SHORT).show();
                                             }
                                         }
-                                    }
+                                    }*/
                                     Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
                                 }
                             }
